ebdfd2f8d61c43da36cca01bade4656bc1027030
dnl232 <danconsa@hotmail.com>
2024-10-27 17:17:21 +0100

    try sending 200 or 404 without reading the whole request

--- app/Main.hs
+++ app/Main.hs
@@ -5,38 +5,59 @@ module Main (main) where
 import Control.Monad (forever)
 import qualified Data.ByteString.Char8 as BC
 import Network.Socket
-    ( getAddrInfo,
-      accept,
-      bind,
-      listen,
-      socket,
-      close,
-      defaultProtocol,
-      AddrInfo(addrAddress, addrFamily),
-      SocketType(Stream) )
-import Network.Socket.ByteString (send)
+  ( AddrInfo (addrAddress, addrFamily),
+    Socket,
+    SocketType (Stream),
+    accept,
+    bind,
+    close,
+    defaultProtocol,
+    getAddrInfo,
+    listen,
+    socket,
+  )
+import Network.Socket.ByteString (recv, send)
 import System.IO (BufferMode (..), hSetBuffering, stdout)
+import Text.Megaparsec (parse)
+import Parser (requestParser, Req(..), emptyReq, ByteStringWithChars(..))
+
+data Status = SUCCESS | FAIL
 
 main :: IO ()
 main = do
-    hSetBuffering stdout LineBuffering
+  hSetBuffering stdout LineBuffering
 
-    let host = "127.0.0.1"
-        port = "4221"
+  let host = "127.0.0.1"
+      port = "4221"
+
+  BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
+
+  -- Get address information for the given host and port
+  addrInfo <- getAddrInfo Nothing (Just host) (Just port)
+
+  serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
+  bind serverSocket $ addrAddress $ head addrInfo
+  listen serverSocket 5
+
+  -- Accept connections and handle them forever
+  forever $ do
+    (clientSocket, clientAddr) <- accept serverSocket
+    BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
+    -- Handle the clientSocket as needed...
+    req <- getNextData clientSocket >>= either (const $ return emptyReq) return . parse requestParser "" . BS
     
-    BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
-    
-    -- Get address information for the given host and port
-    addrInfo <- getAddrInfo Nothing (Just host) (Just port)
-    
-    serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
-    bind serverSocket $ addrAddress $ head addrInfo
-    listen serverSocket 5
-    
-    -- Accept connections and handle them forever
-    forever $ do
-        (clientSocket, clientAddr) <- accept serverSocket
-        BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
-        -- Handle the clientSocket as needed...
-        _ <- send clientSocket "HTTP/1.1 200 OK\r\n\r\n" 
-        close clientSocket
+    if uri req == "/"
+        then do
+            _ <- send clientSocket "HTTP/1.1 200 OK\r\n\r\n"
+            close clientSocket
+        else do
+            _ <- send clientSocket "HTTP/1.1 404 Not Found\r\n\r\n"
+            close clientSocket
+
+getNextData :: Socket -> IO BC.ByteString
+getNextData s = go s ""
+  where
+    go sInput acc = do
+      next <- recv sInput 1
+      let end = BC.cons (BC.last acc) next
+      if end == "\r\n" then return (BC.concat [acc, next]) else go s (BC.concat [acc, next])
---
+++ app/Parser.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Parser where
+
+import Text.Megaparsec (choice, many, Parsec, satisfy, takeWhileP)
+import Data.Char (isDigit)
+import Text.Megaparsec.Char (string, char)
+import Text.Megaparsec.Stream (Stream(..), ShareInput(..), Token, Tokens)
+import Data.Proxy
+import Data.Void(Void)
+import Data.Functor (($>))
+import Data.Bifunctor (second)
+import qualified Data.ByteString.Char8 as BC
+import Network.HTTP.Types (StdMethod(..), HttpVersion(..), http09)
+
+newtype ByteStringWithChars = BS BC.ByteString
+
+instance Stream (ShareInput ByteStringWithChars) where
+  type Token (ShareInput ByteStringWithChars) = Char
+  type Tokens (ShareInput ByteStringWithChars) = BC.ByteString
+  tokenToChunk Proxy = BC.singleton
+  tokensToChunk Proxy = BC.pack
+  chunkToTokens Proxy = BC.unpack
+  chunkLength Proxy = BC.length
+  chunkEmpty Proxy = BC.null
+  take1_ (ShareInput (BS s)) = second (ShareInput . BS) <$> BC.uncons s
+  takeN_ n (ShareInput (BS s))
+    | n <= 0 = Just (BC.empty, ShareInput (BS s))
+    | BC.null s = Nothing
+    | otherwise = Just . second (ShareInput . BS) $ BC.splitAt n s
+  takeWhile_ p (ShareInput (BS s)) = second (ShareInput . BS) $ BC.span p s
+
+instance Stream ByteStringWithChars where
+  type Token ByteStringWithChars = Token (ShareInput ByteStringWithChars)
+  type Tokens ByteStringWithChars = Tokens (ShareInput ByteStringWithChars)
+  tokenToChunk Proxy = tokenToChunk (Proxy :: Proxy (ShareInput ByteStringWithChars))
+  tokensToChunk Proxy = tokensToChunk (Proxy :: Proxy (ShareInput ByteStringWithChars))
+  chunkToTokens Proxy = chunkToTokens (Proxy :: Proxy (ShareInput ByteStringWithChars))
+  chunkLength Proxy = chunkLength (Proxy :: Proxy (ShareInput ByteStringWithChars))
+  chunkEmpty Proxy = chunkEmpty (Proxy :: Proxy (ShareInput ByteStringWithChars))
+  take1_ s = second unShareInput <$> take1_ (ShareInput s)
+  takeN_ n s = second unShareInput <$> takeN_ n (ShareInput s)
+  takeWhile_ p s = second unShareInput $ takeWhile_ p (ShareInput s)
+
+type MethodParser = Parsec Void ByteStringWithChars StdMethod
+
+type HTTPVersionParser = Parsec Void ByteStringWithChars HttpVersion
+
+type URIParser = Parsec Void ByteStringWithChars BC.ByteString
+
+getParser :: MethodParser
+getParser = string "GET" $> GET
+
+postParser :: MethodParser
+postParser = string "POST" $> POST
+
+patchParser :: MethodParser
+patchParser = string "PATCH" $> PATCH
+
+putParser :: MethodParser
+putParser = string "PUT" $> PUT
+
+optionsParser :: MethodParser
+optionsParser = string "OPTIONS" $> OPTIONS
+
+deleteParser :: MethodParser
+deleteParser = string "DELETE" $> DELETE
+
+connectParser :: MethodParser
+connectParser = string "CONNECT" $> CONNECT
+
+traceParser :: MethodParser
+traceParser = string "TRACE" $> TRACE
+
+headParser :: MethodParser
+headParser = string "HEAD" $> HEAD
+
+methodParser :: MethodParser
+methodParser = choice [getParser, postParser, patchParser, putParser, optionsParser, deleteParser, connectParser, traceParser, headParser]
+
+httpVersionParser :: HTTPVersionParser
+httpVersionParser = string "HTTP/" *> many (satisfy isDigit) >>= (\major -> char '.' *> many (satisfy isDigit) >>= (\minor -> pure $ HttpVersion (maybe (error "Unexpected") fst . BC.readInt $ BC.pack major) (maybe (error "Unexpected") fst . BC.readInt $ BC.pack minor)))
+
+uriParser :: URIParser
+uriParser = takeWhileP Nothing (\tok -> tok `elem` ['\r', '\n', ' '])
+
+data Req = Req {
+    method :: StdMethod,
+    uri :: BC.ByteString,
+    httpVersion :: HttpVersion
+}
+
+type ReqParser = Parsec Void ByteStringWithChars Req 
+
+requestParser :: ReqParser
+requestParser = do
+    m <- methodParser
+    _ <- char ' '
+    u <- uriParser
+    _ <- char ' '
+    v <- httpVersionParser
+    _ <- string "\r\n"
+    return $ Req{method = m, uri = u, httpVersion = v}
+
+emptyReq :: Req
+emptyReq = Req {method=GET, uri="", httpVersion = http09 }
